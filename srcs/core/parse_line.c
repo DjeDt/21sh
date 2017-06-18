fe7fe84c1b15596b9e9f162312b999b85569cbba
DjeDt <djelali.dinaut@gmail.com>
2017-06-19 00:59:27 +0200

    test

--- srcs/core/parse_line.c
+++ srcs/core/parse_line.c
@@ -12,55 +12,6 @@
 
 #include "sh21.h"
 
-static	int		nbr_mots(const char *str, char c)
-{
-	int i;
-	int nombremot;
-
-	i = 0;
-	nombremot = 0;
-	while (str[i] != '\0')
-	{
-		while (str[i] == c)
-			i++;
-		if (str[i] != '\0')
-		{
-			nombremot++;
-			while ((str[i] != '\0') && (str[i] != c))
-				i++;
-		}
-	}
-	return (nombremot);
-}
-
-char			**split_line(const char *s, char c)
-{
-	int		i;
-	int		j;
-	int		taille;
-	int		nbmot;
-	char	**ret;
-
-	i = 0;
-	j = -1;
-	taille = 0;
-	if (s == NULL)
-		return (NULL);
-	nbmot = nbr_mots(s, c);
-	if (!(ret = (char**)malloc(sizeof(char*) * nbmot + 1)))
-		return (NULL);
-	while (++j < nbmot)
-	{
-		while (s[i] != '\0' && s[i] == c)
-			i++;
-		taille = ft_strnlen(s + i, c);
-		ret[j] = ft_strndup(s + i, taille);
-		i += taille;
-	}
-	ret[j] = NULL;
-	return (ret);
-}
-
 char	**parse_input(char *line)
 {
 	char **ret;
